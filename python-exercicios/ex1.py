2c9103c04711b4599a090bd32f28738d94d641da
David <moreira.david2015@gmail.com>
2025-07-31 19:03:46 -0300

    commit-1

---
+++ python-exercicios/ex1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print(f"A soma dos dois números digitados é {soma}!")
+subtracao = num1 - num2
+print(f"A subtração dos dois números digitados é {subtracao}!")
+multi = num1 * num2
+print(f"A multiplicação dos dois números digitados é {multi}!")
+divisao = num1 / num2
+print(f"A divisão dos dois números digitados é {divisao}!")
